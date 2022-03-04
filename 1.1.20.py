2e9e6f5918acbadf0b77b50bfc2b3c942c872c30
john <john@spaceda.space>
2022-03-04 15:16:04 -0500

    complete first problem

--- 1.1.20.py
+++ 1.1.20.py
@@ -1,4 +1,17 @@
 """EXERCISE 1.1.20"""
 
 # Write a recursive static method that computes the value of ln (n!)
+import math
+
+
+def get_ln(n):
+    # ln(1) = 0
+    if n == 1:
+        return 0
+    else:
+        # ln(m*n) = ln(n) + ln(m)
+        return math.log(n) + get_ln(n-1)
+
+
+
 
